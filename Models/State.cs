f50021b4d17c2a439e7e8c59f2fb80b483e54568
Kruxo <142524330+Kruxo@users.noreply.github.com>
2024-12-12 13:56:04 +0100

    ..

--- Models/State.cs
+++ Models/State.cs
@@ -1,7 +1,7 @@
 namespace MvcBasics.Models;
 
 // Represents a state with a name and a list of cities
-public class State(string name, List<City> cities)
+public class State(string name, List<City> cities) //Using primary constructor and also properties from City.cs in our list
 {
     public string Name { get; } = name;
     public List<City> Cities { get; } = cities;
